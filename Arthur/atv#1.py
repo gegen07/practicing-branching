94f05b99762d9c98e69af5d0d2cfac4f7d20e9c2
Arthur Ataíde <114112558+Arthur-Ataide@users.noreply.github.com>
2022-10-21 14:33:51 -0300

    Update atv#1.py

--- Arthur/atv#1.py
+++ Arthur/atv#1.py
@@ -1,1 +1,13 @@
+v = [1, 4, 6, 2, 3 , 5]
 
+trocou = 1
+while(trocou):
+    trocou = 0
+    for i in range(len(v)-1):
+        if v[i] > v[i+1]:
+            aux = v[i]
+            v[i] = v[i+1]
+            v[i+1] = aux
+            trocou = 1
+            
+print(v)
