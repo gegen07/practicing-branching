f34df0334c02f8918efe99776ff253669c8d54a9
Maria Eduarda de Pinho Braga <79584320+Duds04@users.noreply.github.com>
2022-10-21 14:09:32 -0300

    Create atividade3.py

---
+++ duds04/atividade3.py
@@ -0,0 +1,10 @@
+print("Você prestou atenção no minicurso?\n")
+print("(Digite S para sim e N para não)\n")
+print("Resposta: \n")
+r = input()
+
+
+if("N"):
+    print("Vishhh vai dar não\n")
+else:
+    print("Parabens voce brilhou ( ´ ∀ `)ノ～ ☆\nAgora voce esta treinado em git ja pode deletar a main")
