0abdbe50c8e72a13f576e9b0f352da26d3ae06ea
Vitor Pontes <vitor.pontes@ufv.br>
2022-10-21 14:37:15 -0300

    Atividade02 add

--- main.c
+++ main.c
@@ -16,10 +16,27 @@ void bubble_sort (int vetor[], int n) {
         }
     }
 }
+void somaCumulativa(int vetor[],int n){
+    int i;
+    int array2[n];
+
+    for (i = 0; i < n; i++){
+        if(i == 0){
+            array2[0] = vetor[0];
+            printf("Array2[%d] = %d\n",i,array2[i]);
+
+        }else{
+            array2[i] = array2[i-1] + vetor[i];
+            printf("Array2[%d] = %d\n",i,array2[i]);
+        }
+
+    }
+}
 
 int main() {
-    int n;
-    int array[n];
-    bubble_sort(array,n);
+    int n = 6;
+    int array[6] = {5,6,7,8,9,10};
+    //bubble_sort(array,n);
+    somaCumulativa(array,n);
     return 0;
 }
