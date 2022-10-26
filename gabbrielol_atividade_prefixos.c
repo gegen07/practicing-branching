7c66192e58b5f6ce6f447b136bc3bc1052975362
Gabriel Rodrigues <gabriel.r.marques@ufv.br>
2022-10-26 14:35:54 -0300

    Update gabbrielol_atividade_prefixos.c

---
+++ gabbrielol_atividade_prefixos.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+    int array[5] = {5, 10, 15, 20, 25};
+    for (i = 0; i < 5; i++) {
+        printf("%d ", array[i]);
+    }
+    for (i = 0; i < 5; i++) {
+        array[i] = array[i] + array[i - 1];
+    }
+    printf("\n");
+    for (i = 0; i < 5; i++) {
+        printf("%d ", array[i]);
+    }
+    return 0;
+}
