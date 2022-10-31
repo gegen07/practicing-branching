97affa544567edbc384bc53e6d154faddc0a49e9
Bryan <bryans0701@gmail.com>
2022-10-31 10:13:50 -0300

    [ativ1] removendo return desnecessário

--- ibryans/bubble-sort.py
+++ ibryans/bubble-sort.py
@@ -5,14 +5,13 @@ def change_values(list, v1, v2):
     temp = list[v1]
     list[v1] = list[v2]
     list[v2] = temp
-    return list
 
 def bubble_sort(list):
     size = len(list)-1
     for passnum in range(size, 0, -1):
         for i in range(passnum):
             if list[i] > list[i+1]:
-                list = change_values(list, i, i+1)
+                change_values(list, i, i+1)
 
 list_example = [54, 26, 93, 17, 77, 31, 44, 55, 20]
 
